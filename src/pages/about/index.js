24119e5dc3b6d78956fd4a385cee3b1539689590
usama <malikusamaa36@gmail.com>
2023-02-20 22:33:38 +0500

    update files

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -11,11 +11,11 @@ export default function About (){
        <Box textAlign={"center"} width={"80%"} height={"70vh"} mx="auto" pt="13vh">
        <Flex direction={{base:"column",md:"row"}} justifyContent="space-evenly" alignItems={"center"}>
         
-        <Image src="/dummy.jpg" h={"200px"} w={"200px"} borderRadius="50%"/>
+        <Image src="/dummy.jpg" h={"200px"} w={"200px"} borderRadius="50%" alt="image"/>
         <Box width={{base:"100%",md:"60%"}} mt="20px"  textAlign={"start"}>
         <Heading>Malik Usama Bin Nusrat</Heading> 
         <Text fontSize='22px' textAlign={"justify"}>I build this todo app in next.js with Chakra Ui. 
-        I enroll web 3 and metaverse course in PIAIC. I'm MERN stack developer. 
+        I enroll web 3 and metaverse course in PIAIC. I&apos; m MERN stack developer. 
         Currently i am working as a react.js developer at software company.</Text>
         </Box>
         </Flex>
